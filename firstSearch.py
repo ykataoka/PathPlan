da981700ca74422802a52b67047b846a172eed87
ykataoka <yk1002jp@gmail.com>
2017-10-06 14:27:43 -0700

    sanitize and change user name...

--- firstSearch.py
+++ firstSearch.py
@@ -1,35 +1,34 @@
-# -----------
-# User Instructions:
+# ----------
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
 #
-# Modify the the search function so that it returns
-# a shortest path as follows:
-# 
-# [['>', 'v', ' ', ' ', ' ', ' '],
-#  [' ', '>', '>', '>', '>', 'v'],
-#  [' ', ' ', ' ', ' ', ' ', 'v'],
-#  [' ', ' ', ' ', ' ', ' ', 'v'],
-#  [' ', ' ', ' ', ' ', ' ', '*']]
-#
-# Where '>', '<', '^', and 'v' refer to right, left, 
-# up, and down motions. Note that the 'v' should be 
-# lowercase. '*' should mark the goal cell.
-#
-# You may assume that all test cases for this function
-# will have a path from init to goal.
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
 # ----------
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
 
-# initial position
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+grid = [[0, 1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
 init = [0, 0]
-
-# goal position
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
+
+# record the step at which each cell is checked
+expand = [[-1 for _col in range(len(grid[0]))] for _row in range(len(grid))]
+count = 0  # the number of step
 
 delta = [[-1, 0],  # go up
          [0, -1],  # go left
@@ -38,65 +37,83 @@ delta = [[-1, 0],  # go up
 
 delta_name = ['^', '<', 'v', '>']
 
+
 def search(grid, init, goal, cost):
-    # ----------------------------------------
-    # modify code below
-    # ----------------------------------------
-    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+
+    # define the array for 'done or not'
+    closed = [[0 for _col in range(len(grid[0]))] for _row in range(len(grid))]
     closed[init[0]][init[1]] = 1
-    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
     action = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
 
-    x = init[0]
-    y = init[1]
-    g = 0
+    row, col = init
+    g = 0  # g-value (the number of step)
+    expand[row][col] = 0
     count = 1
-    open = [[g, x, y]]
+    open = [[g, row, col]]
 
-    found = False  # flag that is set when search is complete
-    resign = False # flag set if we can't find expand
+    # define flag
+    flg_found = False  # when search is done
+    flg_resign = False  # when dead end is found
 
-    while not found and not resign:
+    # loop until the either of flg becomes true
+    while not flg_found and not flg_resign:
+
+        # if dead end, return 'fail'
         if len(open) == 0:
-            resign = True
+            flg_resign = True
             return 'fail'
+
         else:
+            # sort the candidates and take out the least g-value one
             open.sort()
-            open.reverse()
-            next = open.pop()
-            x = next[1]
-            y = next[2]
+            next = open.pop(0)
+            row = next[1]
+            col = next[2]
             g = next[0]
-            expand[x][y] = count
+
+            # update expand (step matrix)
+            expand[row][col] = count
             count += 1
 
-            
-            if x == goal[0] and y == goal[1]:
-                found = True
+            # complete condition
+            if row == goal[0] and col == goal[1]:
+                flg_found = True
+                # pp.pprint(expand)
+
+            # else, add the next step to open
             else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = 1
-                            action[x2][y2] = i
+                for i in range(len(delta)):  # apply all the pattern
+                    row2 = row + delta[i][0]
+                    col2 = col + delta[i][1]
 
-    policy = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
-    x = goal[0]
-    y = goal[1]
-    policy[x][y] = '*'
-    while x != init[0] or y != init[1]:
-        x2 = x - delta[action[x][y]][0]
-        y2 = y - delta[action[x][y]][1]
-        policy[x2][y2] = delta_name[action[x][y]]
-        x = x2
-        y = y2
+                    # edge condition
+                    if (row2 >= 0 and row2 <= (len(grid)-1)
+                            and col2 >= 0 and col2 <= (len(grid[0])-1)):
 
-    return policy # make sure you return the shortest path
+                        # new possible step
+                        if closed[row2][col2] == 0 and grid[row2][col2] == 0:
+                            g2 = g + cost  # update g-value
+                            open.append([g2, row2, col2])  # add new step
+                            closed[row2][col2] = 1  # update closed
+                            action[row2][col2] = i  # record delta type which led to new place
+
+    # Visulize the path history
+    policy = [[' ' for _r in range(len(grid[0]))] for _c in range(len(grid))]
+    row, col = goal
+    policy[row][col] = '*'
+
+    # search in backward!!
+    while row != init[0] or col != init[1]:
+        row2 = row - delta[action[row][col]][0]
+        col2 = col - delta[action[row][col]][1]
+        policy[row2][col2] = delta_name[action[row][col]]
+        row = row2
+        col = col2
+
+    pp.pprint(policy)
+
+    return [g, row2, col2]
 
 
-for ex in search(grid, init, goal, cost):
-    print(ex)
+if __name__ == '__main__':
+    search(grid, init, goal, cost)
